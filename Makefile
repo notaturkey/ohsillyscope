06752252ed7606d69cd5f4462d7c8b094fd969b6
notaturkey <tommytocool5@gmail.com>
2023-03-24 16:32:47 -0600

    init

---
+++ Makefile
@@ -0,0 +1,18 @@
+CFLAGS=-Wall -O3 -g -Wextra -Wno-unused-parameter
+CXXFLAGS=$(CFLAGS)
+
+# Where our library resides. You mostly only need to change the
+# RGB_LIB_DISTRIBUTION, this is where the library is checked out.
+RGB_LIB_DISTRIBUTION=matrix
+RGB_INCDIR=$(RGB_LIB_DISTRIBUTION)/include
+RGB_LIBDIR=$(RGB_LIB_DISTRIBUTION)/lib
+RGB_LIBRARY_NAME=rgbmatrix
+RGB_LIBRARY=$(RGB_LIBDIR)/lib$(RGB_LIBRARY_NAME).a
+LDFLAGS+=-L$(RGB_LIBDIR) -Lgame/lib/ -lgame -l$(RGB_LIBRARY_NAME) -lrt -lm -lpthread -lncurses
+
+
+osc: osc.o
+	$(CXX) $(CXXFLAGS) osc.o -o $@ -lasound $(LDFLAGS) $(RGB_LDFLAGS)
+
+osc.o: osc.cc
+	$(CXX) -I$(RGB_INCDIR) $(CXXFLAGS) osc.cc -c -o $@ -lasound $(LDFLAGS) $(RGB_LDFLAGS) 
---
+++ osc.cc
@@ -0,0 +1,179 @@
+//   open_the_device();
+//   set_the_parameters_of_the_device();
+//   while (!done) {
+//        /* one or both of these */
+//        receive_audio_data_from_the_device();
+//    deliver_audio_data_to_the_device();
+//   }
+//   close the device
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <alsa/asoundlib.h>
+        
+#include "led-matrix.h"
+
+#include <unistd.h>
+#include <math.h>
+#include <signal.h>
+#include <iostream>
+#include <vector>
+#include <utility>
+
+#include <chrono>
+#include <thread>
+
+using rgb_matrix::RGBMatrix;
+using rgb_matrix::Canvas;
+
+int bpm = 80;
+using namespace std;
+volatile bool interrupt_received = false;
+static void InterruptHandler(int signo) {
+  interrupt_received = true;
+}
+
+
+static void DrawOnCanvas(Canvas *canvas, short buf[], float r, float g, float b){
+    canvas->Fill(0, 0, 0);
+    for(int i= 0; i<64; i++){
+        canvas->SetPixel(i, 32+int(buf[2*i]/514), int(100*r), int(100*g), int(100*b));
+    }
+}
+
+main (int argc, char *argv[])
+{
+    // matrix is hardcoded for 64x64 unless someone wants to fix that
+    RGBMatrix::Options defaults;
+    defaults.hardware_mapping = "adafruit-hat";  // or e.g. ""
+    defaults.rows = 64;
+    defaults.cols = 64;
+    defaults.chain_length = 1;
+    defaults.parallel = 1;
+    Canvas *canvas = RGBMatrix::CreateFromFlags(&argc, &argv, &defaults);
+    if (canvas == NULL)
+        return 1;
+
+    
+
+    int i;
+    int err;
+    short buf[128];
+    snd_pcm_t *capture_handle;
+    snd_pcm_hw_params_t *hw_params;
+    unsigned int dumb = 10;
+    unsigned int* test = &dumb;
+    if ((err = snd_pcm_open (&capture_handle, argv[1], SND_PCM_STREAM_CAPTURE, 0)) < 0) {
+        fprintf (stderr, "cannot open audio device %s (%s)\n", 
+                argv[1],
+                snd_strerror (err));
+        exit (1);
+    }
+        
+    if ((err = snd_pcm_hw_params_malloc (&hw_params)) < 0) {
+        fprintf (stderr, "cannot allocate hardware parameter structure (%s)\n",
+                snd_strerror (err));
+        exit (1);
+    }
+                
+    if ((err = snd_pcm_hw_params_any (capture_handle, hw_params)) < 0) {
+        fprintf (stderr, "cannot initialize hardware parameter structure (%s)\n",
+                snd_strerror (err));
+        exit (1);
+    }
+
+    if ((err = snd_pcm_hw_params_set_access (capture_handle, hw_params, SND_PCM_ACCESS_RW_INTERLEAVED)) < 0) {
+        fprintf (stderr, "cannot set access type (%s)\n",
+                snd_strerror (err));
+        exit (1);
+    }
+
+    if ((err = snd_pcm_hw_params_set_format (capture_handle, hw_params, SND_PCM_FORMAT_S16_LE)) < 0) {
+        fprintf (stderr, "cannot set sample format (%s)\n",
+                snd_strerror (err));
+        exit (1);
+    }
+
+    if ((err = snd_pcm_hw_params_set_rate_near (capture_handle, hw_params, test, 0)) < 0) {
+        fprintf (stderr, "cannot set sample rate (%s)\n",
+                snd_strerror (err));
+        exit (1);
+    }
+
+    if ((err = snd_pcm_hw_params_set_channels (capture_handle, hw_params, 2)) < 0) {
+        fprintf (stderr, "cannot set channel count (%s)\n",
+                snd_strerror (err));
+        exit (1);
+    }
+
+    if ((err = snd_pcm_hw_params (capture_handle, hw_params)) < 0) {
+        fprintf (stderr, "cannot set parameters (%s)\n",
+                snd_strerror (err));
+        exit (1);
+    }
+
+    snd_pcm_hw_params_free (hw_params);
+
+    if ((err = snd_pcm_prepare (capture_handle)) < 0) {
+        fprintf (stderr, "cannot prepare audio interface for use (%s)\n",
+                snd_strerror (err));
+        exit (1);
+    }
+
+
+    float r = 0;
+    float g = 0;
+    float b = 0;
+    bool rup = true;
+    bool bup = true;
+    bool gup = true;
+    while (true) {
+        if (rup){
+            r = r+.01;
+        }
+        else{
+            r = r-.01;
+        }
+        if (bup){
+            b = b+.001;
+        }else{
+            b = b-.001;
+        }
+        if (gup){
+            g = g+.025;
+        }else{
+            g = g-.025;
+        }
+
+        if (r >= 1){
+            rup = false;
+        }
+        if (r <= 0 ){
+            rup = true;
+        }
+        if (b >= 1){
+            bup = false;
+        }
+        if (b <= 0){
+            bup = true;
+        }
+        if (g >= 1){
+            gup = false;
+        }
+        if (g <= 0){
+            gup = true;
+        }
+        if ((err = snd_pcm_readi (capture_handle, buf, 128)) != 128) {
+            fprintf (stderr, "read from audio interface failed (%s)\n",
+                    snd_strerror (err));
+            exit (1);
+        }
+        DrawOnCanvas(canvas, buf, buf2, buf3,r,g,b);
+        signal(SIGTERM, InterruptHandler);
+        signal(SIGINT, InterruptHandler);
+    }
+    
+    snd_pcm_close (capture_handle);
+    canvas->Clear();
+    exit (0);
+}
